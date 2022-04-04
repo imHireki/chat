ba260f8cd6ef7b6e6d4a3fdc968b871ee882e3a8
imHireki <imhireki@gmail.com>
2022-04-04 16:25:11 -0300

    [refactor]: Improve the JWTAuthMiddleware

--- backend/core/asgi.py
+++ backend/core/asgi.py
@@ -6,11 +6,18 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
-
 import os
 
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.tokens import AccessToken
+from rest_framework_simplejwt.models import TokenUser
+
 from django.core.asgi import get_asgi_application
-from channels.auth import AuthMiddlewareStack
+from django.utils.functional import LazyObject
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AnonymousUser
+
+from channels.middleware import BaseMiddleware
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.db import database_sync_to_async
 
@@ -19,40 +26,53 @@ import apps.chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-from django.contrib.auth import get_user_model
-from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework_simplejwt.tokens import AccessToken
-from rest_framework_simplejwt.models import TokenUser
+
+@database_sync_to_async
+def get_user(scope):
+    try:
+        headers = dict(scope.get('headers'))
+        authorization = headers.get(b'authorization')
+
+        token = AccessToken(authorization.decode().split(' ')[1])
+        user_id = TokenUser(token).id
+        user = get_user_model().objects.get(id=user_id)
+
+    except Exception as UnauthenticatedUser:
+        user = AnonymousUser()
+
+    return user
 
 
-class JWTAuthMiddleware:
-    def __init__(self, inner):
-        self.inner = inner
+class UserLazyObject(LazyObject):
+    """
+    Throw a more useful error message when scope['user'] is accessed before
+    it's resolved
+    """
 
-    @database_sync_to_async
-    def get_user(self, id):
-        return get_user_model().objects.get(id=id)
+    def _setup(self):
+        raise ValueError("Accessing scope user before it is ready.")
+
+
+class JWTAuthMiddleware(BaseMiddleware):
+    def populate_scope(self, scope):
+        if "user" not in scope:
+            scope["user"] = UserLazyObject()
+
+    async def resolve_scope(self, scope):
+        scope["user"]._wrapped = await get_user(scope=scope)
 
     async def __call__(self, scope, receive, send):
-
         # Copy scope to stop changes going upstream
         scope = dict(scope)
 
-        try:
-            headers = dict(scope.get('headers'))
-            authorization = headers.get(b'authorization')
+        # Scope injection/mutation per this middlware's needs.
+        self.populate_scope(scope)
 
-            token = AccessToken(authorization.decode().split(' ')[1])
-            user_id = TokenUser(token).id
-
-            scope['user'] = await self.get_user(user_id)
-
-        except Exception as auth_exception:
-            #print(auth_exception)
-            pass
+        # Grab the finalized/resolved scope
+        await self.resolve_scope(scope)
 
         # Run the inner application along with the scope
-        return await self.inner(scope, receive, send)
+        return await super().__call__(scope, receive, send)
 
 
 application = ProtocolTypeRouter({
